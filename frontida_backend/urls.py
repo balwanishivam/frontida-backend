0b163b9ab97a25b841276023da864ecaa4800a6e
Shivam Balwani <201851122@iiitvadodara.ac.in>
2020-08-21 00:41:43 +0530

    Urls Edited

--- frontida_backend/urls.py
+++ frontida_backend/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
-    path('api/',include('medical_store.urls')),
+    path('medical-store/',include('medical_store.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
