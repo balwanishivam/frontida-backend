badc228f39518e4cb3f40b237516a95fff47470f
Shivam Balwani <201851122@iiitvadodara.ac.in>
2020-08-28 02:31:51 +0530

    Users Api

--- Users/urls.py
+++ Users/urls.py
@@ -4,5 +4,5 @@ app_name="Users"
 
 urlpatterns = [
      path('add-details/',UserDetailCreate.as_view(),name="add-details"),
-
+     path('get-details/',UserDetailsGet.as_view(),name="get-details"),
 ] 
