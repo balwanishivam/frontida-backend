9de796f868643cf112ae2af7cdd3af3e974605d7
Kshittiz Bhardwaj <59140501+Kshittiz21@users.noreply.github.com>
2020-09-22 03:20:46 +0530

    Serializers.py updated

--- medical_store/serializers.py
+++ medical_store/serializers.py
@@ -1,23 +1,52 @@
 from rest_framework.serializers import ModelSerializer
 from .models import *
 
-class MedicineSerializers(ModelSerializer):
+class MedicineInventorySerializers(ModelSerializer):
     class Meta:
         model=MedicineInventory
+        fields = ['batch_number', 'medicine_name', 'company_name', 'mfd', 'expiry', 'purchase_price', 'sale_price', 'medicine_quantity']
         exclude=['account', 'HSNcode',]
 
-
-class BillingSerializers(ModelSerializer):
+class CompanyDetailsSerializers(ModelSerializer):
     class Meta:
-        model=Billing
-        # exclude=['account',]
+        model = CompanyDetails
+        fields = ['company_name', 'company_contact', 'company_address', 'company_email', 'gst_number']
+        exclude = ['account']
 
-class AccountSerializers(ModelSerializer):
+class Purchase(ModelSerializer):
     class Meta:
-        model=Accounting
-        # exclude=['account',]
+        model = Purchase
+        fields = ['distributor_name', 'company_name', 'bill_number', 'bill_date', 'total_amount', 'discount']
+        exclude = ['account']
+
+class PurchaseInventory(ModelSerializer):
+    class Meta:
+        model = PurchaseInventory
+        fields = ['medicine_name', 'quantity', 'batch_number', 'price_of_each', 'purchase_id']
+
+class Sales(ModelSerializer):
+    class Meta:
+        model = Sales
+        fields = ['customer_name', 'customer_contact', 'referred_by', 'bill_date', 'total_amount', 'discount']
+        exclude = ['account']
+
+class SalesInventory(ModelSerializer):
+    class Meta:
+        model = SalesInventory
+        fields = ['medicine_name', 'quantity', 'batch_number', 'price_of_each', 'sales_id']
+        exclude = ['account']
+
+#class BillingSerializers(ModelSerializer):
+#    class Meta:
+#        model=Billing
+#        # exclude=['account',]
+
+#class AccountSerializers(ModelSerializer):
+#    class Meta:
+#        model=Accounting
+#        # exclude=['account',]
     
-class DeliverySerializers(ModelSerializer):
-    class Meta:
-        model=Delivery
-        # exclude=['account',]
+#class DeliverySerializers(ModelSerializer):
+#    class Meta:
+#        model=Delivery
+#        # exclude=['account',]
