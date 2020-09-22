94aaa3c921ea2cb101bde54866e665a011045c36
Shivam Balwani <201851122@iiitvadodara.ac.in>
2020-09-22 13:23:55 +0530

    Model Error rectified

--- medical_store/models.py
+++ medical_store/models.py
@@ -10,9 +10,9 @@ from authentication.models import User
 #Medicine Inventory
 class MedicineInventory(models.Model):
     HSNcode=models.CharField(max_length=6, default='3004', blank=True)
-    batch_number=models.CharField(max_length=20,min_length=5)
+    batch_number=models.CharField(max_length=20)
     medicine_name = models.CharField(max_length=200)
-    company_name = models.ForeignKey('CompanyDetails', max_length=200)
+    company_name = models.ForeignKey('CompanyDetails',on_delete=models.DO_NOTHING)
     mfd = models.DateField(null=False)
     expiry = models.DateField(null=False)
     purchase_price = models.PositiveIntegerField()
@@ -23,12 +23,12 @@ class MedicineInventory(models.Model):
 #     #create_buy and update_buy
 
 class CompanyDetails(models.Model):
-    company_name = models.CharField(min_length=1,max_length=200)
+    company_name = models.CharField(max_length=200)
     company_contact = models.PositiveIntegerField(validators=[MaxValueValidator(9999999999),MinValueValidator(1000000000)])
-    company_address=models.CharField(max_lenght=200)
+    company_address=models.CharField(max_length=200)
     company_email = models.EmailField()
     gst_number = models.CharField(max_length=15)
-    account = models.ForeignKey('User',on_delete=models.CASCADE)
+    account = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.company_name
@@ -36,12 +36,12 @@ class CompanyDetails(models.Model):
 
 class Purchase(models.Model):
     distributor_name = models.CharField(max_length=50)
-    company_name = models.ForeignKey('CompanyDetails', max_length=200)
-    bill_number= models.CharField()
+    company_name = models.ForeignKey('CompanyDetails',on_delete=models.DO_NOTHING)
+    bill_number= models.CharField(max_length=10)
     bill_date= models.DateTimeField()
-    total_amount= models.DecimalField(decimal_places=2)
-    discount = models.DecimalField(decimal_places=2, max_length=4)
-    account = models.ForeignKey('User',on_delete=models.CASCADE)
+    total_amount= models.DecimalField(decimal_places=2, max_digits=10)
+    discount = models.DecimalField(decimal_places=2, max_digits=4)
+    account = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return  self.id
@@ -50,7 +50,7 @@ class Purchase(models.Model):
 class PurchaseInventory(models.Model):
     medicine_name = models.CharField(max_length=200)
     quantity = models.IntegerField()
-    batch_number = models.CharField(max_length=20,min_length=5)
+    batch_number = models.CharField(max_length=20)
     price_of_each= models.PositiveIntegerField()
     purchase_id= models.ForeignKey('Purchase', on_delete=models.DO_NOTHING)
 
@@ -59,9 +59,9 @@ class Sales(models.Model):
     customer_contact = models.PositiveIntegerField(validators=[MaxValueValidator(9999999999),MinValueValidator(1000000000)])
     referred_by = models.CharField(max_length=50)
     bill_date= models.DateTimeField()
-    total_amount= models.DecimalField(decimal_places=2)
-    discount = models.DecimalField(decimal_places=2, max_length=4)
-    account = models.ForeignKey('User',on_delete=models.CASCADE)
+    total_amount= models.DecimalField(decimal_places=2, max_digits=10)
+    discount = models.DecimalField(decimal_places=2, max_digits=4)
+    account = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
@@ -71,7 +71,7 @@ class SalesInventory(models.Model):
     medicine_name = models.CharField(max_length=200)
     quantity = models.IntegerField()
     #prescription = models.CharField(max_length=)
-    batch_number = models.CharField(max_length=20,min_length=5)
+    batch_number = models.CharField(max_length=20)
     price_of_each= models.PositiveIntegerField()
     sales_id = models.ForeignKey('Sales', on_delete=models.DO_NOTHING)
 
