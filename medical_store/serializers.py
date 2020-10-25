2ba7d66df8e9b9d9c051577ae7e212cae17a3d22
Shivam Balwani <201851122@iiitvadodara.ac.in>
2020-10-25 15:47:58 +0530

    Sales Contact Added

--- medical_store/serializers.py
+++ medical_store/serializers.py
@@ -41,13 +41,18 @@ class SalesInventorySerializers(ModelSerializer):
     class Meta:
         model = SalesInventory
         exclude = ['sales_id', 'isexpired'] 
+    
 
 class SalesSerializers(ModelSerializer):
     salesinventory = SalesInventorySerializers(many=True)
     class Meta:
         model = Sales
         fields = ['customer_name', 'customer_contact', 'referred_by', 'bill_date', 'total_amount', 'discount', 'salesinventory']
-    
+
+    def validate(self,attrs):
+        if attrs.get('customer_contact') not in range(6000000000, 9999999999):
+            return Response({'error': 'Invalid contact number'})
+        return super().validate(attrs)
     def create(self, validated_data):
         sales_inventory_validated = validated_data.pop('salesinventory')
         sales = Sales.objects.create(**validated_data)
