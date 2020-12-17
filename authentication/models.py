49353f6031a153a9b339282ecced3ba3154319a4
Krishna Gaur <gaurkrishna498@gmail.com>
2020-12-18 00:34:02 +0530

    Added required setting GeoDjango

--- authentication/models.py
+++ authentication/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from django.contrib.gis.geos import Point
 import datetime
 from django.core.validators import MaxValueValidator, MinValueValidator
 USER_TYPE=[
