cb9bf3abdee6e3ef2da7aba928a0e1ee941f8d82
Krishna Gaur <gaurkrishna498@gmail.com>
2020-12-18 21:39:05 +0530

    Added location field to user model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import User, UserDetails
+from leaflet.admin import LeafletGeoAdmin
+
 admin.site.register(User)
-admin.site.register(UserDetails)
-# Register your models here.
+admin.site.register(UserDetails, LeafletGeoAdmin)
