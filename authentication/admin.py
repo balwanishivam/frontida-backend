3ff31576a034b3f737b6bec6f10b9425a7a69d01
Krishna Gaur <gaurkrishna498@gmail.com>
2020-12-19 20:52:45 +0530

    Added google maps to django admin panel

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import User, UserDetails
-from leaflet.admin import LeafletGeoAdmin
+from django.contrib.gis.db.models import GeometryField
+from django.forms.widgets import TextInput
+from mapwidgets.widgets import GooglePointFieldInlineWidget
+
+
+
+class PointLocation(admin.ModelAdmin):
+    formfield_overrides = {
+        GeometryField: {'widget': GooglePointFieldInlineWidget}
+    }
 
 admin.site.register(User)
-admin.site.register(UserDetails, LeafletGeoAdmin)
+admin.site.register(UserDetails, PointLocation)
