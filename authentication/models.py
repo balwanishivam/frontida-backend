5abe3e67e21e29fbff0984623e696414b9b3cadd
Krishna Gaur <gaurkrishna498@gmail.com>
2020-12-27 20:46:32 +0530

    Added Point field to user Model

--- authentication/models.py
+++ authentication/models.py
@@ -91,7 +91,7 @@ class UserDetails(models.Model):
     city = models.CharField(max_length=50, choices=CITY)
     pincode = models.PositiveIntegerField()
     contact = models.BigIntegerField()
-    # point = models.PointField(geography=True, default=Point(0.0, 0.0))
+    point = models.PointField(geography=True, default=Point(0.0, 0.0))
     account = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
