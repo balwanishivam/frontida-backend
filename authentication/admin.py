b4ba06acaa8f53bb1b0ec5a9fc9149c33451d8b1
Krishna Gaur <gaurkrishna498@gmail.com>
2020-12-27 17:52:06 +0530

    Updated admin.py

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from .models import User, UserDetails
-from django.contrib.gis.db.models import GeometryField
-from django.forms.widgets import TextInput
-from mapwidgets.widgets import GooglePointFieldInlineWidget
+# from django.contrib.gis.db.models import GeometryField
+# from django.forms.widgets import TextInput
+# from mapwidgets.widgets import GooglePointFieldInlineWidget
+from leaflet.admin import LeafletGeoAdmin
 
 
-
-class PointLocation(admin.ModelAdmin):
-    formfield_overrides = {
-        GeometryField: {'widget': GooglePointFieldInlineWidget}
-    }
+# class PointLocation(admin.ModelAdmin):
+#     formfield_overrides = {
+#         GeometryField: {'widget': GooglePointFieldInlineWidget}
+#     }
 
 admin.site.register(User)
-admin.site.register(UserDetails, PointLocation)
+admin.site.register(UserDetails, LeafletGeoAdmin)
