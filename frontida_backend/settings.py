da3fd0a2904f2ea47db07ac1b85632cb87fc790e
Shivam Balwani <201851122@iiitvadodara.ac.in>
2021-02-13 16:20:01 +0530

    settings updated

--- frontida_backend/settings.py
+++ frontida_backend/settings.py
@@ -23,7 +23,7 @@ load_dotenv(find_dotenv())
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECRET_KEY = "n@g5nm$#)r7p(enpdsyl#vk7v!x+s80j7t9kli9ngj@1&x56gy"
 if os.name == "nt":
     import platform
